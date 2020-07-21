b9fe59e968b8a1f67b1d09ef92ede5259e71f9d7
nriserEG <nriser@evergage.com>
2020-07-20 19:14:24 -0700

    Revamp Hero Banner template to new standards and looks

--- hero-banner-cta/template.ts
+++ hero-banner-cta/template.ts
@@ -1,28 +1,35 @@
+export class ColorTheme {
+    label: string;
+    className: string;
+}
+
 export class HeroBannerCtaTemplate implements CampaignTemplateComponent {
 
     @title('Background Image URL')
-    imageURL: string;
-    
+    imageURL: string = "https://www.northerntrailoutfitters.com/dw/image/v2/BDPX_PRD/on/demandware.static/-/Library-Sites-NTO-SFRASharedLibrary/default/dw37eb7010/images/homepage/home-banner-hero-04-1905-600.jpg"
+
     @hidden(true)
-    contentZone: string = "Homepage Hero";
+    contentZone: string = "Homepage | Hero";
+  
+    @options([
+        {label: "Dark text on light background", className: "evg-dark-on-light"},
+        {label: "Light text on dark background", className: "evg-light-on-dark"}
+    ])
+    colorTheme: ColorTheme; 
 
-    style: "Dark on Light" | "Light on Dark";
+    header: string = "New Arrivals in Men's";
 
-    @richText(true)
-    header: string;
-
-    @richText(true)
-    subheader: string;
+    subheader: string = "Check out the latest styles in jackets, footwear, and more!";
 
     @title('CTA Text')
-    ctaText: string;
+    ctaText: string = "SHOP NOW";
 
     @title('CTA Destination URL')
     @subtitle("Enter a fully qualified destination URL for the CTA (e.g., https://www.northerntrailoutfitters.com)")
-    ctaUrl: string;
+    ctaUrl: string = "/default/men";
 
     run(context:CampaignComponentContext) {
         return {};
-    }
-    
+        }
+
 }
