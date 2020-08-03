e9dca2922de09e14302536285efbdef89d62c0e2
Andy Maskiell <amaskiell@evergage.com>
2020-08-03 17:55:06 -0400

    Additional changes to Product Recs

--- product-recs/client.js
+++ product-recs/client.js
@@ -1,25 +1,25 @@
 (function() {
 
-  function apply(context, template, render) {
-      var contentZone = Evergage.getContentZoneSelector(context.contentZone);
-      if (contentZone) {
-          var html = template(context);
-          Evergage.cashDom(contentZone).after(html);
-      }
-  }
+    function apply(context, template, render) {
+        var contentZone = Evergage.getContentZoneSelector(context.contentZone);
+        if (contentZone) {
+            var html = template(context);
+            Evergage.cashDom(contentZone).after(html);
+        }
+    }
 
-  function reset(context, template) {
-      Evergage.cashDom("#evg-product-recs").remove();
-  }
+    function reset(context, template) {
+        Evergage.cashDom("[data-evg-experience-id=" + context.experience + "]").remove();
+    }
 
-  function control() {
+    function control() {
+        // TODO
+    }
 
-  }
-
-  registerTemplate({
-      apply: apply,
-      reset: reset,
-      control: control
-  });
+    registerTemplate({
+        apply: apply,
+        reset: reset,
+        control: control
+    });
 
 })();
