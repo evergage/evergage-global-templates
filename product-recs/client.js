3eabc357c3dbaae8755297777bcb9d1b2eca18ed
Andy Maskiell <amaskiell@evergage.com>
2020-08-04 17:28:41 -0400

    Refine Configuration settings, add comments to HTML and clientside code

--- product-recs/client.js
+++ product-recs/client.js
@@ -14,8 +14,33 @@
         });
     }
 
-    function apply(context, template, render) {
-        var contentZone = Evergage.getContentZoneSelector(context.contentZone);
+    function apply(context, template) {
+
+        /**
+         * The code below replaces the content of your selected content zone
+         * with the HTML generated from Handlebars.
+         *
+         * If you instead wish to insert your generated HTML before or after your
+         * selected content zone, use .before(html) or .after(html), respectively.
+         *
+         * Note: To use a content zone, you must define a "contentZone" configuration
+         * property in the Serverside Code.
+         *
+         * Use Evergage.DisplayUtils.pageElementLoaded to defer the rendering of
+         * the template until the content zone element is loaded on page.
+         * The observer element that monitors for the content zone element to get inserted
+         * into its DOM node is set to "body" by default. For performance optimization, this
+         * default can be overridden by adding a second selector argument, which will be used
+         * as the observer element instead.
+         *
+         * Visit the Display Utilities documentation to learn more:
+         * https://developer.evergage.com/templates/display-utilities
+         *
+         * Note: To use Evergage.DisplayUtils.pageElementLoaded, you must have the
+         * Display Utilities gear installed and enabled for your dataset.
+         */
+        var contentZone = Evergage.getContentZoneSelector(context.contentZone.name);
+        console.log(contentZone)
         if (contentZone) {
             var html = template(context);
             Evergage.cashDom(contentZone).after(html);
@@ -24,11 +49,28 @@
     }
 
     function reset(context, template) {
+
+        /** Remove the template from the DOM to reset the template. */
         Evergage.cashDom(buildExperienceSelector(context)).remove();
+
     }
 
-    function control() {
-        // TODO
+    function control(context) {
+
+        /**
+         * Add Evergage data attributes to elements you wish to track in the control experience.
+         *
+         * Note: Using Evergage data attributes allows you to track stats with Evergage and
+         * requires the Campaign Stat Tracking gear installed and enabled for your dataset.
+         * Visit the Campaign Stats Tracking documentation to learn more:
+         * https://developer.evergage.com/templates/campaign-stats
+         */
+        var selector = Evergage.getContentZoneSelector(context.contentZone);
+        Evergage.cashDom(selector).attr("data-evg-campaign-id", context.campaign);
+        Evergage.cashDom(selector).attr("data-evg-experience-id", context.experience);
+        Evergage.cashDom(selector).attr("data-evg-user-group", "Control");
+        Evergage.cashDom(selector + " a").attr("data-evg-clickthrough", "");
+
     }
 
     registerTemplate({
--- product-recs/template.ts
+++ product-recs/template.ts
@@ -1,12 +1,31 @@
 import { RecommendationsConfig, RecipeReference, RecipeReferenceLookup, recommend } from "recs";
 
+export class ProductRecommendationsContentZones {
+    name: string;
+    label: string;
+}
+
 export class ProductRecommendationsTemplate implements CampaignTemplateComponent {
 
-    @hidden(true)
-    contentZone: string = "homepage_product_recommendations";
-
-    /** Multiple content zone options */
-    // contentZone: "Homepage | Product Recommendations" | "PDP Recs Row 1" | "PDP Recs Row 2";
+    /**
+     * Business-User Controls
+     */
+    // @hidden(true)
+    // contentZone: string = "homepage_product_recommendations";
+    @options([
+        { name: "homepage_product_recommendations", label: "Homepage Product Recommendations" },
+        { name: "product_detail_recs_row_1", label: "Product Details Recommendations Row 1" },
+        { name: "product_detail_recs_row_2", label: "Product Details Recommendations Row 2" },
+        // TODO: Needed in sitemap
+        // { name: "plp_recs_row", label: "PLP Recommendations Row" },
+        // { name: "cart_recs_row_1", label: "Cart Recommendations Row 1" },
+        // { name: "cart_recs_row_2", label: "Cart Recommendations Row 2" },
+        // { name: "order_confirmation_recs_row", label: "Order Confirmation Recommendations Row" },
+        // { name: "arc_modal_recs_row", label: "Arc Modal Recommendations Row" },
+        // { name: "404_error_recs_row", label: "404 Error Recommendations Row" },
+        // { name: "search_results_recs_row", label: "Search Results Recommendations Row" },
+    ])
+    contentZone: ProductRecommendationsContentZones;
 
     @title("Recommendations Row Header")
     header: string;
@@ -14,7 +33,6 @@ export class ProductRecommendationsTemplate implements CampaignTemplateComponent
     @lookupOptions((self) => new RecipeReferenceLookup("Product"))
     @title("Recipe")
     recipeId: RecipeReference;
-    maxResults: number = 6;
 
     @title("Show Product name")
     // @shownIf()
@@ -31,6 +49,12 @@ export class ProductRecommendationsTemplate implements CampaignTemplateComponent
     @title("Show Product rating")
     // @shownIf()
     ratingVisibility: boolean = true;
+
+    /**
+     * Developer Controls
+     */
+    @hidden(true)
+    maxResults: 4 | 5 | 6 = 6;
 
     @hidden(true)
     visibilityOptions: object;
