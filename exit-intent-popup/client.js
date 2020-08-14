4a800a5961e78d048b1e6ea8038f3f2d0c519646
nriserEG <nriser@evergage.com>
2020-08-14 13:54:21 -0700

    Combine templates: EVG-11869 EVG-11868 EVG-11870 EVG-11867

---
+++ exit-intent-popup/client.js
@@ -0,0 +1,35 @@
+(function() {
+
+    function apply(context, template) {
+        Evergage.DisplayUtils.pageExit(2000).then(function() {
+                context.overlayClass = context.lightboxEnabled ? "evg-overlay" : "";
+                var html = template(context);
+                Evergage.cashDom("body").append(html);
+
+            /** Dismisses popup */
+            const dismissSelectors = [
+                "#evg-exit-intent-popup .evg-overlay",
+                "#evg-exit-intent-popup .evg-btn-dismissal",
+            ];
+
+            Evergage.cashDom(dismissSelectors.join(", ")).on("click", () => {
+                Evergage.cashDom("#evg-exit-intent-popup").remove();
+            });
+        });
+    }
+
+    function reset(context, template) {
+        Evergage.cashDom("#evg-exit-intent-popup").remove();
+    }
+
+    function control() {
+ 
+    }
+  
+    registerTemplate({
+      apply: apply,
+      reset: reset, 
+      control: control
+    });
+
+})();
---
+++ recipe-content-recommendations/client.js
@@ -0,0 +1,39 @@
+(function() {
+
+    function apply(context, template) {
+        /**
+         * The pageElementLoaded method waits for the content zone to load into the DOM
+         * before rendering the template. The observer element that monitors for the content 
+         * zone element to get inserted into its DOM node is set to "body" by default. 
+         * For performance optimization, this default can be overridden by adding
+         * a second selector argument, which will be used as the observer element instead.
+         * 
+         * Visit the Template Display Utilities documentation to learn more:
+         * https://developer.evergage.com/templates/display-utilities
+         */
+        const contentZoneSelector = Evergage.getContentZoneSelector(context.contentZone);
+        return Evergage.DisplayUtils.pageElementLoaded(contentZoneSelector).then(element => {
+            const html = template(context);
+            Evergage.cashDom(element).html(html);
+        });
+    }
+
+    function reset(context, template) {
+        Evergage.cashDom("#evg-recipe-content-recommendations").remove();
+    }
+
+    function control(context) {
+        const contentZoneSelector = Evergage.getContentZoneSelector(context.contentZone);
+        Evergage.cashDom(contentZoneSelector).attr("data-evg-campaign-id", context.campaign);
+        Evergage.cashDom(contentZoneSelector).attr("data-evg-experience-id", context.experience);
+        Evergage.cashDom(contentZoneSelector).attr("data-evg-user-group", "Control");
+        Evergage.cashDom(`${contentZoneSelector} a`).attr("data-evg-clickthrough", "");
+    }
+
+    registerTemplate({
+        apply: apply,
+        reset: reset,
+        control: control
+    });
+
+})();
