01aa720d336c8fb9f6ca6487b0979e04ea00ab03
jsantacruzEG <jsantacruz@evergage.com>
2020-08-14 10:34:53 -0400

    latest revisions based on Exit Intent Popup with Email Capture

--- exit-intent-popup/client.js
+++ exit-intent-popup/client.js
@@ -1,28 +1,29 @@
 (function() {
 
     function apply(context, template) {
-        Evergage.DisplayUtils.pageExit(2000).then(function () {
-            var html = template(context);
-            Evergage.cashDom("body").append(html);
+        //Evergage.DisplayUtils.pageExit(2000).then(function() {
+                context.overlayClass = context.lightboxEnabled ? "evg-overlay" : "";
+                var html = template(context);
+                Evergage.cashDom("body").append(html);
 
-             /** Dismisses popup */
-            const dismissSeletors = [
-                "#evg-email-capture-popup .evg-overlay",
-                "#evg-email-capture-popup .evg-btn-dismissal"
+            /** Dismisses popup */
+            const dismissSelectors = [
+                "#evg-exit-intent-popup .evg-overlay",
+                "#evg-exit-intent-popup .evg-btn-dismissal",
             ];
-            Evergage.cashDom(dismissSeletors.join(", ")).on("click", () => {
-                Evergage.cashDom("#evg-email-capture-popup").remove();
+
+            Evergage.cashDom(dismissSelectors.join(", ")).on("click", () => {
+                Evergage.cashDom("#evg-exit-intent-popup").remove();
             });
-        });
+        //});
     }
 
     function reset(context, template) {
-        var popup = Evergage.cashDom("#evg-email-capture-popup");
-        if (popup) popup.remove();
+        Evergage.cashDom("#evg-exit-intent-popup").remove();
     }
 
     function control() {
-    
+ 
     }
   
     registerTemplate({
@@ -30,5 +31,5 @@
       reset: reset, 
       control: control
     });
-    
-  })();
+
+})();
--- exit-intent-popup/template.ts
+++ exit-intent-popup/template.ts
@@ -1,37 +1,50 @@
 export class StyleField {
     label: string;
-    styleClass: string;
+    className: string;
 }
 
-export class EmailCapturePopupTemplate implements CampaignTemplateComponent {
-    
-    @title('Background Image URL')
-    imageUrl: string = "https://cumulus-fs.s3.amazonaws.com/images/banking-savings.png"
-    
-    @options([
-        {label: "Light text on dark background", styleClass: "evg-light-on-dark"},
-        {label: "Dark text on light background", styleClass: "evg-dark-on-light"}
-    ])
-    style: StyleField = {label: "Dark text on light background", styleClass: "evg-dark-on-light"};
+export class LightboxField {
+    label: boolean;
+    overlayClass: string
+}
 
-    header: string = "Wait, don't leave!";
+export class ExitIntentPopupTemplate implements CampaignTemplateComponent {
 
-    @subtitle('Input sub-header text')
-    
-    subheader: string = "Grow your money with a Cumulus account.";
+    @title("Title")
+    @subtitle("i.e. Don't leave yet!")
+    @richText(true)
+    title: string = "Wait, don't leave!";
+
+    @title("Subtitle")
+    @subtitle("i.e. Grow your money with a Cumulus account.")
+    @richText(true)
+    subtitle: string = "Grow your money with a Cumulus account.";
 
     @title('CTA Text')
+    @subtitle("i.e. Learn More")
     ctaText: string ="Learn More";
 
     @title('CTA Destination URL')
     @subtitle("Enter a fully qualified destination URL for the CTA (e.g., https://www.cumulusfinserv.com/)")
-    ctaUrl: string = "/banking/checking-and-savings";
+    ctaUrl: string = "https://cumulusfinserv.com/banking/checking-and-savings";
+
+    @title("Lightbox")
+    lightboxEnabled: boolean = true;
+
+    @options([
+        {label: "Light on Dark", className: "evg-light-on-dark"},
+        {label: "Dark on Light", className: "evg-dark-on-light" }
+    ])
+    style: StyleField = {label: "Light on Dark", className: "evg-dark-on-light"}
+
+    @title("Background Image URL")
+    imageUrl: string = "https://cdn.evergage.com/evergage-content/cumulus/cumulus_growth.jpg";
 
     @hidden(true)
-    contentZone = "global_popup";
+    contentZone = "global_popup"
 
     run(context: CampaignComponentContext) {
         return {};
     }
-    
+
 }
