77b24fe9a29e05a50b2e327961b9d9efcbdc0859
nriserEG <nriser@evergage.com>
2020-09-17 01:41:16 -0700

    V2 updates for Hero Banner CTA template

--- hero-banner-cta/client.js
+++ hero-banner-cta/client.js
@@ -1,52 +1,40 @@
 (function() {
 
-  /**
-   * @function applyColorTheme
-   * @param {Object} context
-   * @description Sets the color theme to the Color Theme option selected.
-   */
-  function applyColorTheme(context) {
-    Evergage.cashDom("#evg-hero-banner, #evg-hero-banner .evg-h1, #evg-hero-banner .evg-h2")
-    .addClass(context.colorTheme.className);
-  }
+    function apply(context, template) {
+        var selector = Evergage.getContentZoneSelector(context.contentZone);
 
-  function apply(context, template) {
-    var selector = Evergage.getContentZoneSelector(context.contentZone);
+        /**
+         * The pageElementLoaded method waits for the content zone to load into the DOM
+         * before rendering the template. The observer element that monitors for the content 
+         * zone element to get inserted into its DOM node is set to "body" by default. 
+         * For performance optimization, this default can be overridden by adding
+         * a second selector argument, which will be used as the observer element instead.
+         * 
+         * Visit the Template Display Utilities documentation to learn more:
+         * https://developer.evergage.com/templates/display-utilities
+         */
+        return Evergage.DisplayUtils.pageElementLoaded(selector).then(element => {
+            var html = template(context);
+            Evergage.cashDom(element).html(html);
+        });
+    }
 
-    /**
-     * The pageElementLoaded method waits for the content zone to load into the DOM
-     * before rendering the template. The observer element that monitors for the content 
-     * zone element to get inserted into its DOM node is set to "body" by default. 
-     * For performance optimization, this default can be overridden by adding
-     * a second selector argument, which will be used as the observer element instead.
-     * 
-     * Visit the Template Display Utilities documentation to learn more:
-     * https://developer.evergage.com/templates/display-utilities
-     */
-    
-    return Evergage.DisplayUtils.pageElementLoaded(selector).then(function(element) {
-      var html = template(context);
-      Evergage.cashDom(element).html(html);
-      applyColorTheme(context);
+    function reset(context, template) {
+        Evergage.cashDom("#evg-hero-banner").remove();
+    }
+
+    function control(context) {
+        var selector = Evergage.getContentZoneSelector(context.contentZone);
+        Evergage.cashDom(selector).attr("data-evg-campaign-id", context.campaign);
+        Evergage.cashDom(selector).attr("data-evg-experience-id", context.experience);
+        Evergage.cashDom(selector).attr("data-evg-user-group", "Control");
+        Evergage.cashDom(selector + " a").attr("data-evg-clickthrough", "");
+    }
+
+    registerTemplate({
+        apply: apply,
+        reset: reset,
+        control: control
     });
-
-  }
-
-  function reset(context, template) {
-    Evergage.cashDom("#evg-hero-banner").remove();
-  }
-
-  function control(context) {
-    var selector = Evergage.getContentZoneSelector(context.contentZone);
-    Evergage.cashDom(selector).attr("data-evg-campaign-id", context.campaign);
-    Evergage.cashDom(selector).attr("data-evg-experience-id", context.experience);
-    Evergage.cashDom(selector).attr("data-evg-user-group", "Control");
-    Evergage.cashDom(selector + " a").attr("data-evg-clickthrough", "");
-  }
-
-  registerTemplate({
-    apply: apply,
-    reset: reset,
-    control: control
-  });
+    
 })();
--- hero-banner-cta/template.ts
+++ hero-banner-cta/template.ts
@@ -1,32 +1,52 @@
-export class ColorTheme {
+export class StyleField {
     label: string;
     className: string;
 }
 
 export class HeroBannerCtaTemplate implements CampaignTemplateComponent {
 
-    @title('Background Image URL')
-    imageURL: string = "https://www.northerntrailoutfitters.com/dw/image/v2/BDPX_PRD/on/demandware.static/-/Library-Sites-NTO-SFRASharedLibrary/default/dw37eb7010/images/homepage/home-banner-hero-04-1905-600.jpg"
+    @header("Why Use This Template")
+    @headerSubtitle("Easily swap out a hero image and test different headers and CTA copy.")
 
-    @hidden(true)
-    contentZone: string = "home_hero";
+    readonly spacing = " ";
+
+    @header("How to Use This Template")
+    @headerSubtitle(`
+        Enter the image URL for your asset, select your text styling, and then 
+        configure your copy, CTA, and image destination.
+    `)
+
+    @title("Background Image URL")
+    @subtitle("Replace the placeholder image URL with the image URL for your background image.")
+    imageURL: string = "https://cdn.evergage.com/evergage-content/nto/nto_hero_banner_bike.jpg";
   
     @options([
-        {label: "Light text on dark background", className: "evg-light-on-dark"},
-        {label: "Dark text on light background", className: "evg-dark-on-light"}
+        {label: "Light on Dark", className: "evg-light-on-dark"},
+        {label: "Dark on Light", className: "evg-dark-on-light" }
     ])
-    colorTheme: ColorTheme = {label: "Light text on dark background", className: "evg-light-on-dark"};
+    @subtitle("Define header and subheader text styling.")
+    style: StyleField = {label: "Light on Dark", className: "evg-light-on-dark"};  
 
     header: string = "New Arrivals in Men's";
 
+    @title("Header Visibility")
+    headerVisibility: boolean = true;
+
     subheader: string = "Check out the latest styles in jackets, footwear, and more!";
 
-    @title('CTA Text')
+    @title("Subheader Visibility")
+    subheaderVisibility: boolean = true;
+
+    @title("CTA Text")
+    @subtitle("e.g. Shop Now")
     ctaText: string = "SHOP NOW";
 
-    @title('CTA Destination URL')
-    @subtitle("Enter a fully qualified destination URL for the CTA (e.g., https://www.northerntrailoutfitters.com)")
-    ctaUrl: string = "/default/men";
+    @title("CTA Destination URL")
+    @subtitle(`
+        Enter a fully qualified destination URL for the CTA 
+        (e.g. https://www.northerntrailoutfitters.com).
+    `)
+    ctaUrl: string = "https://www.northerntrailoutfitters.com/default/men";
 
     run(context: CampaignComponentContext) {
         return {};
