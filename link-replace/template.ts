690de50544f711f4379b185f6620cfe27f278c13
Andy Maskiell <amaskiell@evergage.com>
2020-09-23 13:15:18 -0400

    Add initial comment to handlebars, change serverside TS according to new requirements

--- link-replace/template.ts
+++ link-replace/template.ts
@@ -1,41 +1,20 @@
 export class PageURLReplacementTemplate implements CampaignTemplateComponent {
 
-    @header("Why Use This Template")
-    @headerSubtitle(`
-        This template will allow you to change the URL of a link (/all links?) on a page to send a visitor to a new
-        experience. This can be used in A/B testing to redirect a user to a different experience based on condition(s)
-        set in the campaign.
-    `)
-
-    readonly spacing1 = " ";
-
-    @header("How To Use This Template")
-    @headerSubtitle(`
-        Add the full URL that exists on the link by default and the URL that the template should replace with the new
-        page or experience to send your visitors. You can create the logic to show this redirect within the campaign.
-    `)
-
-    readonly spacing2 = " ";
-
-    @header("Redirect from URL")
-    @headerSubtitle("This URL should match exactly the URL that is the default on the site.")
-    @title(" ")
+    @title("Current Link URL")
     @placeholder("")
-    @subtitle("i.e. https://www.example.com/example.html")
-    fromUrl: string = ""
+    @subtitle("Current destination URL for selected element (e.g. https://www.example.com/example.html)")
+    currentLinkUrl: string = ""
 
 
-    @header("Redirect to URL")
-    @headerSubtitle("This URL should match exactly the URL that the visitor should go to if all conditions are met.")
-    @title(" ")
+    @title("Updated Link URL")
     @placeholder("")
-    @subtitle("i.e. https://www.example.com/example2.html")
-    toUrl: string = ""
+    @subtitle("New destination URL for selected element (e.g. https://www.example.com/example2.html)")
+    updatedLinkUrl: string = ""
 
     run(context: CampaignComponentContext) {
         return {
-            fromUrl: this.fromUrl,
-            toUrl: this.toUrl
+            currentLinkUrl: this.currentLinkUrl,
+            updatedLinkUrl: this.updatedLinkUrl
         };
     }
 
