8b4043b6f88c89aaf9e40425787fc36245aee3d2
nriserEG <nriser@evergage.com>
2020-10-06 01:31:21 -0700

    EVG-12562: Updates to Google Analytics Segment Push Template

--- google-analytics-segment-push/client.js
+++ google-analytics-segment-push/client.js
@@ -1,52 +1,60 @@
-(function() {
+(function () {
 
-  /**
-   * @function setGaDimension
-   * @description Sets one or more segments, for which users are members of, to each dimension. 
-   * The segment names will be concatenated into one pipe (|) delimited string (e.g. "Segment 1|Segment 2|Segment 3")
-   * when more than one segment per dimension are assigned.
-   */
-  function setGaDimension(context) {
-      const gaMapping = context.gaMapping;
-      for (const dimension in gaMapping) {
-        const matchedSegments = gaMapping[dimension].join("|");
-        window.ga('set', dimension, matchedSegments);
-      }
-  }
-
-  /**
-   * @function sendGaDimensions
-   * @description Sends an event to Google Analytics with segments.
-   */
-  function sendGaDimensions(context) {
-      window.ga("send", {
-        hitType: "event",
-        eventCategory: "Evergage",
-        eventAction: "Set Segments",
-        nonInteraction: true
-      });
-  }
-
-  function apply(context, template) {
-    if (window.ga && typeof window.ga === "function") {
-      setGaDimension(context);
-      sendGaDimensions(context);
-      Evergage.sendStat({campaignStats: [{control: false, experienceId: context.experience, stat: "Impression"}]});
+    /**
+     * @function setGaDimension
+     * @param {Object} context
+     * @description Sets one or more segments, for which users are members of, to each dimension. 
+     * The segment names will be concatenated into one pipe (|) delimited string (e.g. "Segment 1|Segment 2|Segment 3")
+     * when more than one segment per dimension are assigned.
+     */
+    function setGaDimension(context) {
+        const gaMapping = context.gaMapping;
+        for (const dimension in gaMapping) {
+            const matchedSegments = gaMapping[dimension].join("|");
+            window.ga('set', dimension, matchedSegments);
+        }
     }
-  }
 
-  function reset(context, template) {
-  
-  }
-  
-  function control() {
-    
-  }
+    /**
+     * @function sendGaDimensions
+     * @param {Object} context
+     * @description Sends an event to Google Analytics with segments.
+     */
+    function sendGaDimensions(context) {
+        window.ga("send", {
+            hitType: "event",
+            eventCategory: "Evergage",
+            eventAction: "Set Segments",
+            nonInteraction: true
+        });
+    }
 
-  registerTemplate({
-    apply: apply,
-    reset: reset,
-    control: control
-  });
-  
+    function apply(context, template) {
+        if (window.ga && typeof window.ga === "function") {
+            setGaDimension(context);
+            sendGaDimensions(context);
+            Evergage.sendStat({
+                campaignStats: [{
+                    control: false,
+                    experienceId: context.experience,
+                    stat: "Impression"
+                }]
+            });
+        }
+    }
+
+    function reset(context, template) {
+
+    }
+
+    function control() {
+
+    }
+
+    registerTemplate({
+        apply: apply,
+        reset: reset,
+        control: control
+    });
+
 })();
