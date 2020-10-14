a0758a6adf38cd3156db2c08b5b9a59a1fa4366b
Andy Maskiell <amaskiell@evergage.com>
2020-10-14 17:20:28 -0400

    Initial work on Slide-In

---
+++ slide-in-with-cta/client.js
@@ -0,0 +1,44 @@
+(function() {
+
+    function apply(context, template) {
+        switch (context.triggerOptions.name) {
+            case "timeOnPage":
+                break;
+            case "pageScroll":
+                return Evergage.DisplayUtils
+                    .bind("pageScroll")
+                    .pageScroll(context.triggerOptionsNumber)
+                    .then(function(event) {
+                        const html = template(context);
+                        Evergage.cashDom("body").append(html);
+                    });
+            case "inactivity":
+                return Evergage.DisplayUtils
+                    .bind("inactivity")
+                    .pageInactive(context.triggerOptionsNumber)
+                    .then(function(event) {
+                        const html = template(context);
+                        Evergage.cashDom("body").append(html);
+                    });
+        }
+    }
+
+    function reset(context, template) {
+        Evergage.cashDom("[id*=evg-slide-in]").remove();
+    }
+
+    function control(context) {
+        var selector = Evergage.getContentZoneSelector(context.contentZone);
+        Evergage.cashDom(selector).attr("data-evg-campaign-id", context.campaign);
+        Evergage.cashDom(selector).attr("data-evg-experience-id", context.experience);
+        Evergage.cashDom(selector).attr("data-evg-user-group", "Control");
+        Evergage.cashDom(selector + " a").attr("data-evg-clickthrough", "");
+    }
+
+    registerTemplate({
+        apply: apply,
+        reset: reset,
+        control: control
+    });
+
+})();
---
+++ slide-in-with-cta/template.ts
@@ -0,0 +1,90 @@
+export class SlideInTriggerOptions {
+    name: string;
+    label: string;
+}
+
+export class StyleField {
+    label: string;
+    className: string;
+}
+
+export class SlideInWithCTATemplate implements CampaignTemplateComponent {
+
+    @options([
+        {
+            name: "timeOnPage",
+            label: "Time on Page (Delay)"
+        },
+        {
+            name: "pageScroll",
+            label: "Page Scroll"
+        },
+        {
+            name: "inactivity",
+            label: "Inactivity"
+        }
+    ])
+    triggerOptions: SlideInTriggerOptions = { name: "", label: "Select..." };
+
+    @shownIf(this, self => self.triggerOptions.name === "timeOnPage")
+    @title(" ")
+    @subtitle("Seconds on page")
+    secondsOnPage: number = 0;
+
+    @shownIf(this, self => self.triggerOptions.name === "pageScroll")
+    @title(" ")
+    @subtitle("% of page the user has scrolled")
+    percentageScrolled: number = 0;
+
+    @shownIf(this, self => self.triggerOptions.name === "inactivity")
+    @title(" ")
+    @subtitle("Seconds of inactivity on page")
+    secondsInactive: number = 0;
+
+    @hidden(true)
+    triggerOptionsNumber: number;
+
+    @subtitle("Define infobar background & text styling.")
+    @options([
+        { label: "Light on Dark", className: "evg-light-on-dark" },
+        { label: "Dark on Light", className: "evg-dark-on-light" }
+    ])
+    style: StyleField = { label: "Light on Dark", className: "evg-light-on-dark" };
+
+    header: string = "Header Text";
+
+    @title("Header Visibility")
+    headerVisibility: boolean = true;
+
+    subheader: string = "Subheader Text";
+
+    @title("Subheader Visibility")
+    subheaderVisibility: boolean = true;
+
+    @title("CTA Text")
+    ctaText: string = "Call To Action";
+
+    @title("CTA Destination URL")
+    @subtitle("Requires full URL string including https://")
+    ctaUrl: string = "https://www.northerntrailoutfitters.com";
+
+    ctaVisibility: boolean = true;
+
+    run(context: CampaignComponentContext) {
+        switch (this.triggerOptions.name) {
+            case "timeOnPage":
+                this.triggerOptionsNumber = this.secondsOnPage * 1000;
+                break;
+            case "pageScroll":
+                this.triggerOptionsNumber = (this.percentageScrolled / 100);
+                break;
+            case "inactivity":
+                this.triggerOptionsNumber = this.secondsInactive * 1000;
+                break;
+        }
+        return {
+            triggerOptionsNumber: this.triggerOptionsNumber
+        };
+    }
+
+}
