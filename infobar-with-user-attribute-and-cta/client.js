25126e71f1d6879119c9f8d4b3e99596092bd577
Andy Maskiell <amaskiell@evergage.com>
2020-10-21 15:01:23 -0400

    Make requested changes

--- infobar-with-user-attribute-and-cta/client.js
+++ infobar-with-user-attribute-and-cta/client.js
@@ -1,23 +1,11 @@
 (function() {
 
     /**
-     * @function buildTemplateSelector
-     * @param {Object} context
-     * @description Creates unique selector that targets the template.
-     */
-    function buildTemplateSelector(context) {
-        return `[data-evg-campaign-id=${context.campaign}][data-evg-experience-id=${context.experience}]`;
-    }
-
-    /**
      * @function setInfobarPosition
      * @param {Object} context
      * @description Sets the position of the infobar via class assignments, based on content zone selected.
      */
     function setInfobarPosition(context) {
-        context.infobarClass = context.contentZone == "global_infobar_top_of_page"
-            ? "evg-infobar-top"
-            : "evg-infobar-bottom";
         if (context.infobarClass === "evg-infobar-top") {
             Evergage.cashDom("body").css({ "margin-bottom": "0", "margin-top": "2.5rem" });
         } else {
@@ -31,8 +19,8 @@
      * @description Adds click listener to the "X" button that removes the template from the DOM.
      */
     function setDismissal(context) {
-        Evergage.cashDom("#evg-infobar-with-user-attr .evg-btn-dismissal").on("click", () => {
-            Evergage.cashDom(buildTemplateSelector(context)).remove();
+        Evergage.cashDom(`#evg-infobar-with-user-attr${context.infobarClass} .evg-btn-dismissal`).on("click", () => {
+            Evergage.cashDom(`#evg-infobar-with-user-attr${context.infobarClass}`).remove();
             Evergage.cashDom("body").css({ "margin-top": "0", "margin-bottom": "0" });
         });
     }
@@ -42,6 +30,10 @@
         const { firstName } = context.user.attributes;
         const userAttr = firstName || userAttrDefault;
         context.messageText = `${preAttrMessageText}${userAttr}${postAttrMessageText}`;
+        context.infobarClass = context.contentZone == "global_infobar_top_of_page"
+            ? "evg-infobar-top"
+            : "evg-infobar-bottom";
+
         setInfobarPosition(context);
         const html = template(context);
         Evergage.cashDom("body").append(html);
@@ -49,7 +41,7 @@
     }
 
     function reset(context, template) {
-        Evergage.cashDom(buildTemplateSelector(context)).remove();
+        Evergage.cashDom(`#evg-infobar-with-user-attr${context.infobarClass}`).remove();
         Evergage.cashDom("body").css({ "margin-top": "0", "margin-bottom": "0" });
     }
 
