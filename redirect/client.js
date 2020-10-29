20fa41fdd27327f83eaf14c727bdcfc610a65fab
Andy Maskiell <amaskiell@evergage.com>
2020-10-29 09:55:50 -0400

    Remove CSS, refactor clientside JS to use unresolved Promise

--- redirect/client.js
+++ redirect/client.js
@@ -1,51 +1,37 @@
 (function() {
 
-    /**
-     * @function removeTemplateCss
-     * @param {Object} context
-     * @description Removes the inserted style tag inserted by this template
-     */
-    function removeTemplateCss(context) {
-        const selector = `style[evg-experience=${context.experience}][evg-campaign=${context.campaign}]`;
-        if (Evergage.cashDom(selector).length > 0) {
-            Evergage.cashDom(selector).remove();
-        }
-    }
-
     function apply(context, template) {
-        if ((window.frameElement || {}).id === "siteEditorFrame") {
-            removeTemplateCss(context);
+
+        /**
+         * (window.frameElement || {}).id === "siteEditorFrame" is present in order to prevent redirection from
+         * occurring while in either the Template Editor or Campaign Editor.
+         */
+        if ((window.frameElement || {}).id === "siteEditorFrame"
+            || !context.targetPage.includes(window.location.hostname + window.location.pathname)) {
             return;
         }
 
-        if (!context.targetPage.includes(window.location.hostname + window.location.pathname)) {
-            return;
-        }
+        return new Promise((resolve, reject) => {
+            if ((context.targetPage && context.urlForRedirect)
+                && window.location.href !== context.urlForRedirect) {
 
-        clearTimeout(window.evergageReshowPersonalizationsTimeout);
-        window.evergageReshowPersonalizationsTimeout = setTimeout(function() {
-            removeTemplateCss(context);
-        }, (Evergage.getConfig().hideContentSectionsMillis || 2500));
+                Evergage.sendStat({
+                    campaignStats: [
+                        {
+                            control: false,
+                            experienceId: context.experience,
+                            stat: "Impression"
+                        }
+                    ]
+                });
 
-        if ((context.targetPage && context.urlForRedirect)
-            && window.location.href !== context.urlForRedirect) {
-
-            Evergage.sendStat({
-                campaignStats: [
-                    {
-                        control: false,
-                        experienceId: context.experience,
-                        stat: "Impression"
-                    }
-                ]
-            });
-
-            window.location.href = context.urlForRedirect + (context.maintainQueryParams ? window.location.search : "");
-        }
+                window.location.href = context.urlForRedirect + (context.maintainQueryParams ? window.location.search : "");
+            }
+        });
     }
 
     function reset(context, template) {
-        removeTemplateCss(context);
+
     }
 
     function control() {
