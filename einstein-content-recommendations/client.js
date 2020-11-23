74b608b492f2356a03cf68d3d598ae716822931b
Andy Maskiell <amaskiell@evergage.com>
2020-11-23 17:58:39 -0500

    Add bind calls for use of DisplayUtils

--- einstein-content-recommendations/client.js
+++ einstein-content-recommendations/client.js
@@ -1,5 +1,14 @@
 (function() {
 
+    /**
+     * @function buildBindId
+     * @param {Object} context
+     * @description Create unique bind ID based on the campaign and experience IDs.
+     */
+    function buildBindId(context) {
+        return `${context.campaign}:${context.experience}`;
+    }
+
     function apply(context, template) {
         const contentZoneSelector = Evergage.getContentZoneSelector(context.contentZone);
 
@@ -13,20 +22,21 @@
          * Visit the Template Display Utilities documentation to learn more:
          * https://developer.evergage.com/templates/display-utilities
          */
-        return Evergage.DisplayUtils.pageElementLoaded(contentZoneSelector).then(element => {
+        return Evergage.DisplayUtils.bind(buildBindId(context)).pageElementLoaded(contentZoneSelector).then(element => {
             const html = template(context);
             Evergage.cashDom(element).html(html);
         });
     }
 
     function reset(context, template) {
+        Evergage.DisplayUtils.unbind(buildBindId(context));
         Evergage.cashDom(`[data-evg-campaign-id="${context.campaign}"][data-evg-experience-id="${context.experience}"]`)
             .remove();
     }
 
     function control(context) {
         const contentZoneSelector = Evergage.getContentZoneSelector(context.contentZone);
-        return Evergage.DisplayUtils.pageElementLoaded(contentZoneSelector).then(element => {
+        return Evergage.DisplayUtils.bind(buildBindId(context)).pageElementLoaded(contentZoneSelector).then(element => {
             Evergage.cashDom(element).attr({
                 "data-evg-campaign-id": context.campaign,
                 "data-evg-experience-id": context.experience,
