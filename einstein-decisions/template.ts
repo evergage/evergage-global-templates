f7366601006114574cec9d3d08957fa804a95e2e
Andy Maskiell <amaskiell@salesforce.com>
2021-03-03 14:21:27 -0500

    Add business user config text to serverside TS

--- einstein-decisions/template.ts
+++ einstein-decisions/template.ts
@@ -2,6 +2,12 @@ import { ContextualBanditConfig, decide } from "corvus";
 
 export class EinsteinDecisionsTemplate implements CampaignTemplateComponent {
 
+    @header(' ')
+    @headerSubtitle('Only offer assets tagged with the targeted web content zone will be eligible to return in the campaign.')
+
+    @hidden(true)
+    forHeaderSubtitle;
+
     run(context: CampaignComponentContext) {
         const banditConfig: ContextualBanditConfig = {
             maxResults: 1,
